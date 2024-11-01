ffa076b32575650949d14b74e4f1dc6e7913bca6
omar95-pero <omarpero85@gmail.com>
2024-11-01 14:23:55 +0200

    add opening balance

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Artisan::call('migrate');
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
 
         view()->share('settings', Setting::firstOrCreate());
 
