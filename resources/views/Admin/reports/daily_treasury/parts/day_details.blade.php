3c429e14a1fa6def2ea76476f5a74f2599b83ae0
omar95-pero <omarpero85@gmail.com>
2025-01-22 15:38:45 +0200

    add features

---
+++ resources/views/Admin/reports/daily_treasury/parts/day_details.blade.php
@@ -0,0 +1,64 @@
+<h2>تنفيذات المناديب</h2>
+<table id="table" class="table table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%">
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>اجمالي الاوردرات</td>
+            <td>نقدي الاوردرات</td>
+            <td>غير نقدي الاوردرات</td>
+            <td>مصروفات المناديب</td>
+            <td>بنزين</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($dliveryOrders as $key => $order)
+            <tr>
+                <td>{{ ++$key }}</td>
+                <td>{{ $order->total_orders }}</td>
+                <td>{{ $order->cash }}</td>
+                <td>{{ $order->cheque }}</td>
+                <td>{{ $order->fees }}</td>
+                <td>{{ $order->solar }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+<h2> المصروفات الادارية</h2>
+<table id="table" class="table table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%">
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>القيمة</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($expenses as $key => $expense)
+            <tr>
+                <td>{{ ++$key }}</td>
+                <td>{{ $expense->value }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+<h2> مدفوعات التجار</h2>
+<table id="table" class="table table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%">
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>الاجمالي</td>
+            <td>نقدي</td>
+            <td>غير نقدي</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($traderPayments as $key => $payment)
+            <tr>
+                <td>{{ ++$key }}</td>
+                <td>{{ $expense->amount }}</td>
+                <td>{{ $expense->cash }}</td>
+                <td>{{ $expense->cheque }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
